24d7fd8e1a07848e178787022d8d0639d095f101
mohidqadeer123 <mohidqadeer@iu.edu>
2025-10-29 07:19:53 +0500

    def _init

--- apputil.py
+++ apputil.py
@@ -3,8 +3,14 @@ import pandas as pd
 
 def GroupEstimate(object):
     def __init__(self, estimate):
+
+        # Validate type of estimate
+        if estimate not in ["mean","median"]:
+            raise ValueError("estimate should be either 'mean' or 'median'")
         self.estimate = estimate
-    
+        self.group_estimates = None
+        self.group_features = None
+       
     def fit(self, X, y):
         return None
 
